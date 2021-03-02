9ad5de1fac3ff042161ea027b93f90ede0a7ead2
pamelaireri <pamt4510@outlook.com>
2021-03-01 23:02:51 -0800

    adding updated index, css, and script

--- script.js
+++ script.js
@@ -2,11 +2,34 @@ function compute()
 {
    
     var principal = document.getElementById("principal").value;
+    
     var rate = document.getElementById("rate").value;
+    //document.getElementById("rangeVal").innerHTML = rate;
+
     var years = document.getElementById("years").value;
     var interest = (principal * years * rate) / 100;
-    document.getElementById("test").innerHTML=interest;
+    
+    //converting number of years into actual year in future.
+    var futureYear = new Date(); 
+    futureYear.setFullYear(futureYear.getFullYear() + parseInt(years));
+    
+    var text = "<br> If you deposit " + Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(principal) +  
+    "<br> at an interest rate of " + rate + "%." +
+    "<br> You will receive an amount of " + Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(interest) +
+    "<br> in the year " + futureYear.getFullYear() + "."; 
+
+    //displaying result
+    document.getElementById("result").innerHTML = text;
 
 }
-       
+
+//Creating function to display value of slider oninput.
+
+function rangeVal()
+{
+     
+    var val = document.getElementById("rate").value; // getting range input value 
+    document.getElementById("rangeVal").innerHTML = val;    // displaying value
+
+}     
         
